7afb892e42ee0e423bbf0bc3b7624fa41157e41b
divyanshhj <divyansh.jain@bornwest.com>
2026-06-23 00:49:46 +0530

    feat: refactor useIsMobile hook to improve mobile state management

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,19 +3,28 @@ import * as React from "react";
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
-    undefined,
-  );
+  const [isMobile, setIsMobile] = React.useState<boolean>(false);
 
   React.useEffect(() => {
+    if (typeof window === "undefined") return;
+
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+
+    const updateMobileState = () => {
+      requestAnimationFrame(() => {
+        setIsMobile(mql.matches);
+      });
     };
+
+    updateMobileState();
+
+    const onChange = () => {
+      updateMobileState();
+    };
+
     mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     return () => mql.removeEventListener("change", onChange);
   }, []);
 
-  return !!isMobile;
+  return isMobile;
 }
